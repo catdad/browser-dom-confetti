a0c5c8d2ffffce0136ee6a572488a55d3024f9ba
Kiril Vatev <vatev.1@gmail.com>
2018-01-25 20:39:22 -0500

    adding webpack build

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+/* eslint-env node */
+
+var path = require('path');
+
+module.exports = {
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      loader: 'babel-loader',
+      options: {
+        presets: ['es2015']
+      }
+    }]
+  },
+  entry: {
+    index: path.resolve(__dirname, 'src/index.js'),
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js'
+  },
+  target: 'web',
+  resolve: {
+    modules: [ 'node_modules' ],
+    extensions: [ 'js' ]
+  },
+
+  devServer: {
+    https: false,
+    port: 9000
+  }
+};
